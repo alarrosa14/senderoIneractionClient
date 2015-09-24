4a628b157c6993fe285134c541c6586f2a6c1bdd
Diego Ernst <dernst92@gmail.com>
2015-09-23 23:53:02 -0300

    Se agrega soporte para varios clientes

--- src/InteractionBehaviour.cpp
+++ src/InteractionBehaviour.cpp
@@ -31,7 +31,7 @@ std::vector<std::string> split(const std::string &s, char delim) {
 }
 
 InteractionBehaviour::InteractionBehaviour(){
-    radius = 50;
+    radius = 30;
 }
 
 InteractionBehaviour::~InteractionBehaviour(){
@@ -50,38 +50,53 @@ void InteractionBehaviour::update() {
     Envelope::ptr_t env;
     BasicMessage::ptr_t message;
 
-    if (connection->BasicConsumeMessage("consumertag", env, 0))
-    {
-        message = env->Message();
-        std::string coords = message->Body();
-        std::vector<std::string> coord = split(coords,',');
+    for(int i = 0; i < pixelsFast->size(); i++){
+        Pixel* px = (*pixelsFast)[i];
+        // px->blendRGBA(0,0,0,255,1);
+        px->fadeToBlack(0.9);
+    }
 
-        float x = atof(coord[0].c_str());
-        float y = atof(coord[1].c_str());
-        float z = atof(coord[2].c_str());
+    for(int i = 0; i < 5; i++){
 
-        ofVec3f touchPosition(x, y, z); 
+        if (connection->BasicConsumeMessage("consumertag", env, 0)){
 
-        for(int i = 0; i < pixelsFast->size(); i++){
-            Pixel* px = (*pixelsFast)[i];
-            ofVec3f pxPosition = px->getPosition();
+            message = env->Message();
+            std::string coords = message->Body();
+            std::vector<std::string> coord = split(coords,',');
 
-            float dist = touchPosition.distance(pxPosition);
+            float x = atof(coord[0].c_str());
+            float y = atof(coord[1].c_str());
+            float z = atof(coord[2].c_str());
+            int r = atoi(coord[3].c_str());
+            int g = atoi(coord[4].c_str());
+            int b = atoi(coord[5].c_str());
 
-            px->blendRGBA(0,0,0,255,1);
+            ofVec3f touchPosition(x, y, z); 
 
-            if (dist < this->radius){
-                float normalizedDist = 1 - dist/this->radius;
-                px->blendRGBA(254,17,143,255,ofLerp(0.7,1,normalizedDist));
+            for(int i = 0; i < pixelsFast->size(); i++){
+                Pixel* px = (*pixelsFast)[i];
+                ofVec3f pxPosition = px->getPosition();
+
+                float dist = touchPosition.distance(pxPosition);
+
+                // px->blendRGBA(0,0,0,255,1);
+
+                if (dist < this->radius){
+                    float normalizedDist = 1 - dist/this->radius;
+                    px->blendRGBA(r,g,b,255,ofLerp(0.1,1,normalizedDist));
+                }
             }
-        }
 
-    }else{
-        for(int i = 0; i < pixelsFast->size(); i++){
-            Pixel* px = (*pixelsFast)[i];
-            px->fadeToBlack(0.9);
-        }
+        }else{
 
+            // if (i == 0){
+            //     for(int i = 0; i < pixelsFast->size(); i++){
+            //         Pixel* px = (*pixelsFast)[i];
+            //         px->fadeToBlack(0.9);
+            //     }
+            // }
+            break;
+        }
     }
 
 }
